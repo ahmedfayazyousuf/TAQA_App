f2f2ecfb195e421fa931d9abcb5c1b2570c52a6d
ahmedfayazyousuf <95180025+ahmedfayazyousuf@users.noreply.github.com>
2024-02-07 16:24:23 +0400

    o
    o

--- src/Components/SmartLC/index.js
+++ src/Components/SmartLC/index.js
@@ -1,4 +1,7 @@
 import PPTImage from './Images/PPT.png';
+import PPTImageSmall from './Images/PPTSmall.png';
+import VideoImage from './Images/Video.png';
+
 const ProductPortfolio = () => {
 
     return(
@@ -7,13 +10,12 @@ const ProductPortfolio = () => {
                 
                 <div style={{display: 'flex', alignItems: 'center', justifyContent: 'flex-start', height: '45vh', width: '30vw', background: 'red', flexDirection: 'column'}}>
                     <p style={{width: '100%', backgroundColor: 'yellow', fontWeight: '900', fontSize: '20px', padding: '10px', margin: '0'}}>Screen</p>
-                    <div style={{width: '100%', height: '60px', backgroundColor: 'black', margin: '0', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                    <div style={{width: '100%', height: '60px', backgroundColor: 'black', margin: '0', display: 'flex', alignItems: 'center', justifyContent: 'center', padding: '0'}}>
                         <p style={{width: '100%', fontSize: '20px', color: 'grey', margin: '0'}}>AICD's</p>
                     </div>
-                    <div style={{width: '100%', height: '100%', backgroundColor: 'white', margin: '0', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-                        <img style={{ width: '250px'}} src={PPTImage} alt="PPTImage" />
+                    <div style={{width: '100%', height: '30vh', backgroundColor: 'white', margin: '0', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                        <img style={{ height: '170px'}} src={PPTImage} alt="PPTImage" />
                     </div>
-
                 </div>
 
 
@@ -22,19 +24,24 @@ const ProductPortfolio = () => {
                     <div style={{width: '100%', height: '100%', backgroundColor: 'black', margin: '0', display: 'flex'}}>
 
                         <div style={{backgroundColor: 'green', width: '33%', height: '100%', display: 'flex', flexDirection: 'column'}}>
-                            <div style={{width: '100%', height: '60px', backgroundColor: 'black', margin: '0', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-                                <p style={{width: '100%', fontSize: '20px', color: 'grey', margin: '0'}}>AICD's</p>
+                            <div style={{width: '100%', height: '60px', backgroundColor: 'black', margin: '0', display: 'flex', alignItems: 'center', justifyContent: 'center', padding: '0'}}>
+                                <p style={{width: '100%', fontSize: '20px', color: 'grey', margin: '0', padding: '0'}}>AICD's</p>
                             </div>
 
-                            <div style={{width: '100%', height: '100%', backgroundColor: 'white', margin: '0', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-                                <img style={{ width: '250px'}} src={PPTImage} alt="PPTImage" />
+                            <div style={{width: '100%', height: '30vh', backgroundColor: 'white', margin: '0', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                                <img style={{ height: '170px'}} src={PPTImage} alt="PPTImage" />
                             </div>
                         </div>
 
 
-                        <div style={{backgroundColor: 'blue', display: 'flex', width: '34%', height: '100%'}}>
+                        <div style={{backgroundColor: 'green', width: '34%', height: '100%', display: 'flex', flexDirection: 'column'}}>
                             <div style={{width: '100%', height: '60px', backgroundColor: 'black', margin: '0', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
                                 <p style={{width: '100%', fontSize: '20px', color: 'grey', margin: '0'}}>FloFuse</p>
+                            </div>
+
+                            <div style={{width: '100%', height: '30vh', backgroundColor: 'white', margin: '0', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                                <img style={{ height: '170px'}} src={PPTImageSmall} alt="PPTImage" />
+                                <img style={{ height: '170px'}} src={VideoImage} alt="PPTImage" />
                             </div>
                         </div>
 
@@ -44,8 +51,8 @@ const ProductPortfolio = () => {
                                 <p style={{width: '100%', fontSize: '20px', color: 'grey', margin: '0'}}>Case Study</p>
                             </div>
 
-                            <div style={{width: '100%', height: '100%', backgroundColor: 'white', margin: '0', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-                                <img style={{ width: '250px'}} src={PPTImage} alt="PPTImage" />
+                            <div style={{width: '100%', height: '30vh', backgroundColor: 'white', margin: '0', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                                <img style={{ height: '170px'}} src={PPTImage} alt="PPTImage" />
                             </div>
                         </div>
                     </div>
