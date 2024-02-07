77b33baaf5715f2eab1717314ec681d537cd7366
Adithya Suresh <55681300+AdiS0928@users.noreply.github.com>
2024-02-07 13:28:02 +0400

    commit

--- src/Components/ProductPortfolio/index.js
+++ src/Components/ProductPortfolio/index.js
@@ -2,15 +2,24 @@ import ReactPlayer from 'react-player';
 import { useState } from "react";
 import Modal from 'react-bootstrap/Modal';
 import Image from './Images/image.jpg';
+import PDF from './FloCheck Dissolve Datasheet.pdf'
 
 const ProductPortfolio = () => {
     const [showVideoModal, setShowVideoModal] = useState(false);
+    const [showPDFModal, setShowPDFModal] = useState(false);
+
+    function OpenPDF(){
+        setShowPDFModal(true)
+    }
+
+
 
     return(
         <div>
-            <div style={{height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center', background: 'red'}}>
-                <div style={{display: 'flex', alignItems: 'center', background: 'blue', width: '40vw', height: '100vh', flexDirection: 'column'}}>
-                    <div style={{display: 'flex', height: '30vh', width: '40vw', background: 'red'}}>
+            <div style={{height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                <div style={{display: 'flex', alignItems: 'center', width: '40vw', height: '100vh', flexDirection: 'column'}}>
+                    <div style={{display: 'flex', height: '30vh', width: '40vw', justifyContent: 'center', alignItems: 'flex-end'}}>
+                     <p style={{fontSize:'25px', fontWeight: '900'}}>VIdeo 4x</p>
                     </div>
                     <div style={{display: 'flex', height: '70vh', width: '40vw', background: 'grey', justifyContent: 'center'}}>
                         <button style={{borderRadius: '10px', border: 'none', height: '350px', width: '500px', backgroundSize: 'cover', backgroundImage: `url('${Image}')`, backgroundPosition: 'center'}} onClick={() => setShowVideoModal(true)}>
@@ -18,14 +27,15 @@ const ProductPortfolio = () => {
                     </div>
                 </div>
 
-                <div style={{display: 'flex', alignItems: 'center', background: 'green', width: '60vw', height: '100vh', flexDirection: 'column' }}>
-                    <div style={{display: 'flex', height: '30vh', width: '60vw', background: 'green'}}>
+                <div style={{display: 'flex', alignItems: 'center', width: '60vw', height: '100vh', flexDirection: 'column' }}>
+                <div style={{display: 'flex', height: '30vh', width: '40vw', justifyContent: 'center', alignItems: 'flex-end'}}>
+                        <p style={{fontSize:'25px', fontWeight: '900'}}>Spec Sheet</p>
                     </div>
-                    <div style={{display: 'flex', height: '70vh', width: '60vw', background: 'yellow'}}>             
+                    <div style={{display: 'flex', height: '70vh', width: '60vw'}}>             
                         <table style={{width: '60vw'}}>
                         <thead>
                             <tr>
-                            <th>Column 1</th>
+                            <th >Column 1</th>
                             <th>Column 2</th>
                             <th>Column 3</th>
                             <th>Column 4</th>
@@ -33,40 +43,40 @@ const ProductPortfolio = () => {
                         </thead>
                         <tbody>
                             <tr>
-                            <td>Row 1, Cell 1</td>
-                            <td>Row 1, Cell 2</td>
-                            <td>Row 1, Cell 3</td>
-                            <td>Row 1, Cell 4</td>
+                            <td onClick={()=>{OpenPDF()}}>Row 1, Cell 1</td>
+                            <td onClick={()=>{OpenPDF()}}>Row 1, Cell 2</td>
+                            <td onClick={()=>{OpenPDF()}}>Row 1, Cell 3</td>
+                            <td onClick={()=>{OpenPDF()}}>Row 1, Cell 4</td>
                             </tr>
                             <tr>
-                            <td>Row 2, Cell 1</td>
-                            <td>Row 2, Cell 2</td>
-                            <td>Row 2, Cell 3</td>
-                            <td>Row 2, Cell 4</td>
+                            <td onClick={()=>{OpenPDF()}}>Row 2, Cell 1</td>
+                            <td onClick={()=>{OpenPDF()}}>Row 2, Cell 2</td>
+                            <td onClick={()=>{OpenPDF()}}>Row 2, Cell 3</td>
+                            <td onClick={()=>{OpenPDF()}}>Row 2, Cell 4</td>
                             </tr>
                             <tr>
-                            <td>Row 3, Cell 1</td>
-                            <td>Row 3, Cell 2</td>
-                            <td>Row 3, Cell 3</td>
-                            <td>Row 3, Cell 4</td>
+                            <td onClick={()=>{OpenPDF()}}>Row 3, Cell 1</td>
+                            <td onClick={()=>{OpenPDF()}}>Row 3, Cell 2</td>
+                            <td onClick={()=>{OpenPDF()}}>Row 3, Cell 3</td>
+                            <td onClick={()=>{OpenPDF()}}>Row 3, Cell 4</td>
                             </tr>
                             <tr>
-                            <td>Row 4, Cell 1</td>
-                            <td>Row 4, Cell 2</td>
-                            <td>Row 4, Cell 3</td>
-                            <td>Row 4, Cell 4</td>
+                            <td onClick={()=>{OpenPDF()}}>Row 4, Cell 1</td>
+                            <td onClick={()=>{OpenPDF()}}>Row 4, Cell 2</td>
+                            <td onClick={()=>{OpenPDF()}}>Row 4, Cell 3</td>
+                            <td onClick={()=>{OpenPDF()}}>Row 4, Cell 4</td>
                             </tr>
                             <tr>
-                            <td>Row 5, Cell 1</td>
-                            <td>Row 5, Cell 2</td>
-                            <td>Row 5, Cell 3</td>
-                            <td>Row 5, Cell 4</td>
+                            <td onClick={()=>{OpenPDF()}}>Row 5, Cell 1</td>
+                            <td onClick={()=>{OpenPDF()}}>Row 5, Cell 2</td>
+                            <td onClick={()=>{OpenPDF()}}>Row 5, Cell 3</td>
+                            <td onClick={()=>{OpenPDF()}}>Row 5, Cell 4</td>
                             </tr>
                             <tr>
-                            <td>Row 6, Cell 1</td>
-                            <td>Row 6, Cell 2</td>
-                            <td>Row 6, Cell 3</td>
-                            <td>Row 6, Cell 4</td>
+                            <td onClick={()=>{OpenPDF()}}>Row 6, Cell 1</td>
+                            <td onClick={()=>{OpenPDF()}}>Row 6, Cell 2</td>
+                            <td onClick={()=>{OpenPDF()}}>Row 6, Cell 3</td>
+                            <td onClick={()=>{OpenPDF()}}>Row 6, Cell 4</td>
                             </tr>
                         </tbody>
                         </table>
@@ -88,6 +98,22 @@ const ProductPortfolio = () => {
                     playing={true}
                     loop={true}
                     />
+                </Modal.Body>
+            </Modal>
+
+
+            <Modal style={{height:'100%',width:"100%"}} show={showPDFModal} onHide={() => setShowPDFModal(false)}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Video Player</Modal.Title>
+                </Modal.Header>
+                
+                <Modal.Body style={{height:'80vh',width:"100%"}}> 
+                <embed
+                src= {PDF}
+                type="application/pdf"
+                width="100%"
+                height="100%"
+            />
                 </Modal.Body>
             </Modal>
 
