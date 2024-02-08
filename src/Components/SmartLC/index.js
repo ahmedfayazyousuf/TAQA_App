3c93bb45400b51e8c23018acea117244069c9b6b
Adithya Suresh <55681300+AdiS0928@users.noreply.github.com>
2024-02-08 16:27:46 +0400

    Update index.js

--- src/Components/SmartLC/index.js
+++ src/Components/SmartLC/index.js
@@ -42,78 +42,78 @@ const ProductPortfolio = () => {
 
 
     return(
-        <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh', width: '100vw', background: 'red', flexDirection: 'column'}}>
-            <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', height: '45vh', width: '100vw', background: 'blue'}}>
+        <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh', width: '100vw', flexDirection: 'column',backgroundColor:'#005359'}}>
+            <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', height: '45vh', width: '100vw'}}>
                 
-                <div style={{display: 'flex', alignItems: 'center', justifyContent: 'flex-start', height: '45vh', width: '30vw', background: 'red', flexDirection: 'column'}}>
-                    <p style={{width: '100%', backgroundColor: 'yellow', fontWeight: '900', fontSize: '20px', padding: '10px', margin: '0'}}>Screen</p>
-                    <div style={{width: '100%', height: '60px', backgroundColor: 'black', margin: '0', display: 'flex', alignItems: 'center', justifyContent: 'center', padding: '0'}}>
+                <div style={{display: 'flex', alignItems: 'center', justifyContent: 'flex-start', height: '45vh', width: '30vw', flexDirection: 'column'}}>
+                    <p style={{width: '100%',  fontWeight: '900', fontSize: '20px', padding: '10px', margin: '0'}}>Screen</p>
+                    <div style={{width: '100%', height: '60px', margin: '0', display: 'flex', alignItems: 'center', justifyContent: 'center', padding: '0'}}>
                         <p style={{width: '100%', fontSize: '20px', color: 'grey', margin: '0'}}>AICD's</p>
                     </div>
-                    <div style={{width: '100%', height: '30vh', backgroundColor: 'white', margin: '0', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-                        <img style={{ height: '170px'}} onClick={()=>{document.getElementById('load').style.zIndex = '100'}} src={PPTImage} alt="PPTImage" />
+                    <div style={{width: '100%', height: '30vh', backgroundColor: 'white', margin: '0', display: 'flex', alignItems: 'center', justifyContent: 'center',backgroundColor:'#005359'}}>
+                        <img style={{ height: '170px',backgroundColor:'#005359'}} onClick={()=>{document.getElementById('load').style.zIndex = '100'}} src={PPTImage} alt="PPTImage" />
                     </div>
                 </div>
 
 
-                <div style={{display: 'flex', alignItems: 'center', justifyContent: 'flex-start', height: '45vh', width: '70vw', background: 'grey', flexDirection:'column'}}>
-                    <p style={{width: '100%', backgroundColor: 'yellow', fontWeight: '900', fontSize: '20px', padding: '10px', margin: '0'}}>Inflow Control</p>
-                    <div style={{width: '100%', height: '100%', backgroundColor: 'black', margin: '0', display: 'flex'}}>
+                <div style={{display: 'flex', alignItems: 'center', justifyContent: 'flex-start', height: '45vh', width: '70vw', flexDirection:'column'}}>
+                    <p style={{width: '100%',  fontWeight: '900', fontSize: '20px', padding: '10px', margin: '0'}}>Inflow Control</p>
+                    <div style={{width: '100%', height: '100%', margin: '0', display: 'flex'}}>
 
-                        <div style={{backgroundColor: 'green', width: '33%', height: '100%', display: 'flex', flexDirection: 'column'}}>
-                            <div style={{width: '100%', height: '60px', backgroundColor: 'black', margin: '0', display: 'flex', alignItems: 'center', justifyContent: 'center', padding: '0'}}>
+                        <div style={{ width: '33%', height: '100%', display: 'flex', flexDirection: 'column'}}>
+                            <div style={{width: '100%', height: '60px', margin: '0', display: 'flex', alignItems: 'center', justifyContent: 'center', padding: '0'}}>
                                 <p style={{width: '100%', fontSize: '20px', color: 'grey', margin: '0', padding: '0'}}>AICD's</p>
                             </div>
 
-                            <div style={{width: '100%', height: '30vh', backgroundColor: 'white', margin: '0', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-                                <img onClick={()=>{document.getElementById('load2').style.zIndex = '100'}} style={{ height: '170px'}} src={PPTImage} alt="PPTImage" />
+                            <div style={{width: '100%', height: '30vh', backgroundColor: 'white', margin: '0', display: 'flex', alignItems: 'center', justifyContent: 'center',backgroundColor:'#005359'}}>
+                                <img onClick={()=>{document.getElementById('load2').style.zIndex = '100'}} style={{ height: '170px',backgroundColor:'#005359'}} src={PPTImage} alt="PPTImage" />
                             </div>
                         </div>
 
 
-                        <div style={{backgroundColor: 'green', width: '34%', height: '100%', display: 'flex', flexDirection: 'column'}}>
-                            <div style={{width: '100%', height: '60px', backgroundColor: 'black', margin: '0', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                        <div style={{ width: '34%', height: '100%', display: 'flex', flexDirection: 'column'}}>
+                            <div style={{width: '100%', height: '60px', margin: '0', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
                                 <p style={{width: '100%', fontSize: '20px', color: 'grey', margin: '0'}}>FloFuse</p>
                             </div>
 
-                            <div style={{width: '100%', height: '30vh', backgroundColor: 'white', margin: '0', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-                                <img style={{ height: '170px'}} onClick={()=>{document.getElementById('load3').style.zIndex = '100'}} src={PPTImageSmall} alt="PPTImage" />
-                                <img style={{ height: '170px'}} src={VideoImage} alt="PPTImage" />
+                            <div style={{width: '100%', height: '30vh', backgroundColor: 'white', margin: '0', display: 'flex', alignItems: 'center', justifyContent: 'center',backgroundColor:'#005359'}}>
+                                <img style={{ height: '170px',backgroundColor:'#005359'}} onClick={()=>{document.getElementById('load3').style.zIndex = '100'}} src={PPTImageSmall} alt="PPTImage" />
+                                <img style={{ height: '170px',backgroundColor:'#005359'}} src={VideoImage} alt="PPTImage" />
                             </div>
                         </div>
 
 
-                        <div style={{backgroundColor: 'green', width: '33%', height: '100%', display: 'flex', flexDirection: 'column'}}>
-                            <div style={{width: '100%', height: '60px', backgroundColor: 'black', margin: '0', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                        <div style={{ width: '33%', height: '100%', display: 'flex', flexDirection: 'column'}}>
+                            <div style={{width: '100%', height: '60px', margin: '0', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
                                 <p style={{width: '100%', fontSize: '20px', color: 'grey', margin: '0'}}>Case Study</p>
                             </div>
 
-                            <div style={{width: '100%', height: '30vh', backgroundColor: 'white', margin: '0', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-                                <img style={{ height: '170px'}} src={PPTImage} alt="PPTImage" />
+                            <div style={{width: '100%', height: '30vh', backgroundColor: 'white', margin: '0', display: 'flex', alignItems: 'center', justifyContent: 'center',backgroundColor:'#005359'}}>
+                                <img style={{ height: '170px',backgroundColor:'#005359'}} src={PPTImage} alt="PPTImage" />
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
 
-            <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', height: '45vh', width: '100vw', background: 'green'}}>
-                <div style={{display: 'flex', flexDirection:'column',alignItems: 'flex-start', justifyContent: 'center', height: '45vh', width: '30vw', background: 'grey'}}>
-                    <p style={{width: '100%', backgroundColor: 'yellow', fontWeight: '900', fontSize: '20px', padding: '10px'}}>Water Shut</p>
+            <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', height: '45vh', width: '100vw'}}>
+                <div style={{display: 'flex', flexDirection:'column',alignItems: 'flex-start', justifyContent: 'center', height: '45vh', width: '30vw'}}>
+                    <p style={{width: '100%',  fontWeight: '900', fontSize: '20px', padding: '10px'}}>Water Shut</p>
 
-                    <div style={{width: '100%', height: '30vh', backgroundColor: 'white', margin: '0', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-                        <img style={{ height: '170px'}} onClick={()=>{document.getElementById('load4').style.zIndex = '100'}} src={PPTImage} alt="PPTImage" />
-                        <img style={{ height: '170px'}} onClick={()=>{document.getElementById('load5').style.zIndex = '100'}} src={PPTImage} alt="PPTImage" />
+                    <div style={{width: '100%', height: '30vh', backgroundColor: 'white', margin: '0', display: 'flex', alignItems: 'center', justifyContent: 'center',backgroundColor:'#005359'}}>
+                        <img style={{ height: '170px',backgroundColor:'#005359'}} onClick={()=>{document.getElementById('load4').style.zIndex = '100'}} src={PPTImage} alt="PPTImage" />
+                        <img style={{ height: '170px',backgroundColor:'#005359'}} onClick={()=>{document.getElementById('load5').style.zIndex = '100'}} src={PPTImage} alt="PPTImage" />
 
                     </div>
                 </div>
 
-                <div style={{display: 'flex',flexDirection:'column' ,alignItems: 'flex-start', justifyContent: 'center', height: '45vh', width: '70vw', background: 'red'}}>
-                    <p style={{width: '100%', backgroundColor: 'yellow', fontWeight: '900', fontSize: '20px', padding: '10px'}}>Pulse Eight</p>
+                <div style={{display: 'flex',flexDirection:'column' ,alignItems: 'flex-start', justifyContent: 'center', height: '45vh', width: '70vw'}}>
+                    <p style={{width: '100%',  fontWeight: '900', fontSize: '20px', padding: '10px'}}>Pulse Eight</p>
 
 
-                    <div style={{width: '100%', height: '30vh', backgroundColor: 'white', margin: '0', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-                        <img style={{ height: '170px'}} onClick={()=>{document.getElementById('load6').style.zIndex = '100'}} src={PPTImage} alt="PPTImage" />
-                        <img style={{ height: '170px'}} src={VideoImage} alt="PPTImage" />                    
+                    <div style={{width: '100%', height: '30vh', backgroundColor: 'white', margin: '0', display: 'flex', alignItems: 'center', justifyContent: 'center',backgroundColor:'#005359'}}>
+                        <img style={{ height: '170px',backgroundColor:'#005359'}} onClick={()=>{document.getElementById('load6').style.zIndex = '100'}} src={PPTImage} alt="PPTImage" />
+                        <img style={{ height: '170px',backgroundColor:'#005359'}} src={VideoImage} alt="PPTImage" />                    
                     </div>
                 </div>
             </div>
